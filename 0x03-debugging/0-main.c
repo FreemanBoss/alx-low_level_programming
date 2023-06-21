a1e423b40690290887ac81279c3271abe74b40e0
FreemanBoss <ibrahimgbgba800@gmail.com>
2023-06-21 14:54:30 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test a positive or negative function
+ * Return: always zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
