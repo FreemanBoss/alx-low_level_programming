b36cd4d61e0de015f04d883c7a9f37b738cbddc5
FreemanBoss <ibrahimgbgba800@gmail.com>
2023-06-26 14:58:47 -0700

    all taaks

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
