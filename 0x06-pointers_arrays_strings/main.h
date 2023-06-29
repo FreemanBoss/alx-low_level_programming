219b0ad648bb09c566bcc85e6f810f833419e82b
FreemanBoss <ibrahimgbgba800@gmail.com>
2023-06-29 06:56:34 -0700

    7 tasks done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
-char *string_toupper(char *);
+char *string_toupper(char *c);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
