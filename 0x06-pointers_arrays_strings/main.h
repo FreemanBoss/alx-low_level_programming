931ce615033c164da7096633c9a477927c17de4d
FreemanBoss <ibrahimgbgba800@gmail.com>
2023-06-29 05:04:09 -0700

    first task

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+void print_buffer(char *b, int size);
+
+#endif
