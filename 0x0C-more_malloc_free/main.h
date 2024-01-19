503c17d25fd61bc45ee94a1ac84d930e60d48850
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-01-19 06:25:00 +0100

    malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,9 @@ void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strlen(char *s);
+
+
 
 #endif
