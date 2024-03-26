cf50a88032aee48b3d12c969538dfa6af7e2af3e
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-03-26 06:31:54 +0100

    argh

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-int add(int x, int y)                  {
-        return (x + y);
-}
-int mod (int x, int y)
+int add(int x, int y)
 {
-return (x % y);
+	return(x+y);
 }
 int sub(int x, int y)
 {
-        return (x - y);
-}
-
-int div(int x, int y)
-{
-        return (y / y);
+	return (x - y);
 }
 int mul(int x, int y)
 {
-        return (x * y);
+	return (x * y);
+}
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+int mod(int x, int y)
+{
+	return (x % y);
 }
